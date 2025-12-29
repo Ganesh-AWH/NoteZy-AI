125eb61f294aa6a7da297ded32b8cc8f5f4aea3a
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-12-29 21:21:20 +0530

    modified

--- backend/app/utils/embeddings.py
+++ backend/app/utils/embeddings.py
@@ -17,6 +17,7 @@ class EmbeddingModel:
             convert_to_numpy=True,
             normalize_embeddings=True #It improves cosine similarity results
         )
+        
 
     def embed_query(self, query: str) -> np.ndarray:
         embedding = self.model.encode(
@@ -29,38 +30,3 @@ class EmbeddingModel:
 
 
 
-#testing
-# chunks = [
-#     "Machine learning enables computers to learn patterns from data without being explicitly programmed.",
-#     "Embeddings convert text into numerical vectors that capture semantic meaning.",
-#     "Vector databases allow efficient similarity search over high-dimensional embeddings.",
-#     "Photosynthesis is the process by which green plants convert sunlight into chemical energy."
-# ]
-
-# from numpy import dot
-# from numpy.linalg import norm
-
-# embedder = EmbeddingModel()
-# embeddings = embedder.embed_texts(chunks)
-
-# def cosine_similarity(a, b):
-#     return dot(a, b) / (norm(a) * norm(b))
-
-# query = "How do embeddings represent the meaning of text?"
-
-# query_embedding = embedder.embed_query(query)
-# print(embeddings.shape)
-# print(query_embedding.shape)
-
-
-# max_score = 0
-# chunk_index = None
-# for i, emb in enumerate(embeddings):
-#     score = cosine_similarity(query_embedding, emb)
-#     print(f"Chunk {i+1} similarity: {score:.3f}")
-    
-#     if score > max_score:
-#         max_score = score
-#         chunk_index = i        
-    
-# print(chunks[chunk_index])
